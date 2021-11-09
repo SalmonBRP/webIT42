78fca64cbe6d422ea02ffb0be2563c5c22dd4425
SalmonBRP <playxsix@gmail.com>
2021-11-09 13:24:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+	echo "<<< โปรเเกรมสูตรคูณเเม่ 12 >>><br>";
+	$rs = 12;
+	$i = 1;
+	while($i<=5){
+		$s = $rs*$i;
+		echo "12 * $i =  $s <br>";
+		$i++;
+	}
+	echo "<<<ยุติการทำซ้ำ>>>";
+?>
+
+
+</body>
+</html>
